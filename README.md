0d8a8a083f495b225c1ce5676138264c10046e27
Maged Abdelsalam <dagadego@gmail.com>
2020-09-19 09:32:23 -0700

    Refactored some code, added comments, added readme

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# work-day-scheduler
-https://magedabdelsalam.com/work-day-scheduler 
+# 04 Work Day Scheduler
+Write notes for your busy schedule!
+
+## Links
+* [Website](https://magedabdelsalam.com/work-day-scheduler)
+* [Github Repo](https://github.com/magedabdelsalam/work-day-scheduler)
--- script.js
+++ script.js
@@ -1,28 +1,30 @@
+//Grab current time from moment.js
 var dayTime = moment().format("dddd");
 var dateTime = moment().format("MMMM Do");
-var civilianTime = moment().format("h");
-var militaryTime = moment().format("H");
+var milTime = parseInt(moment().format("H"));
 
+//Define work day array
 var civHour = [9,10,11,12,1,2,3,4,5];
 var milHour = [9,10,11,12,13,14,15,16,17];
-var hourNote = ["","","","","","","","",""];
+var currentNote = ["","","","","","","","",""];
 
-var mainEl = $(".container");
+//Display today's date
 var currentDayEl = $("#currentDay");
-var dayInputTextEl = $("input");
-
 currentDayEl.text(dayTime + ", " + dateTime);
 
-var savedNotes = JSON.parse(localStorage.getItem("notes"));
-if (savedNotes){
-    hourNote = savedNotes;
+//If there's stored notes add them to current notes
+var storedNotes = JSON.parse(localStorage.getItem("storedNotes"));
+if (storedNotes){
+    currentNote = storedNotes;
 }
 
+//Go through the work day hour by hour
 for (i=0;i<milHour.length;i++){
-    // Hour
+
+    //Create hour div
     var rowDivEl = $("<div class='row'>");
 
-    // Hour title
+    //Display hour name
     var pEl = $("<p class='col-1'>");
     if (milHour[i] < 12) {
         pEl.text(civHour[i] + "AM");
@@ -31,35 +33,40 @@ for (i=0;i<milHour.length;i++){
     };
     rowDivEl.append(pEl);
 
-    // Hour notes
-    var inputTextEl = $("<input type='text' class='col'>");
-    inputTextEl.attr("data-note", i);
-    rowDivEl.append(inputTextEl);
+    //Display hour notes
+    var textAreaEl = $("<textarea type='text' class='col'>");
+    textAreaEl.attr("data-note", i);
+    rowDivEl.append(textAreaEl);
+    textAreaEl.val(currentNote[i]);
 
-    if (milHour[i] === militaryTime) {
-        inputTextEl.attr("style","background: lightyellow;");
-    } else if (milHour[i] < militaryTime) {
-        inputTextEl.attr("style","background: lightred;");
-    } else if(milHour[i] > militaryTime) {
-        inputTextEl.attr("style","background: lightgreen;");
+    //Change color based on current time
+    if (milHour[i] === milTime) {
+        textAreaEl.attr("style","background: lemonchiffon;");
+    } else if (milHour[i] < milTime) {
+        textAreaEl.attr("style","background: lightcoral;");
+    } else if(milHour[i] > milTime) {
+        textAreaEl.attr("style","background: lightgreen;");
     };
 
-    inputTextEl.val(hourNote[i]);
-
-    // Hour notes save
+    //Display save button
     var inputSubmitEl = $("<input type='submit' value='Save' class='save col-1'>");
     inputSubmitEl.append("<i class='fas fa-save'></i>");
     rowDivEl.append(inputSubmitEl);
     
+    //Append hour elements into main view
+    var mainEl = $(".container");
     mainEl.append(rowDivEl);
+
 }
 
+//Save hour notes when the save button is clicked
 $(".save").click(function () {
-    var noteData = $(this).prev().attr("data-note")
-    console.log($(this).prev().attr("data-note"))
     
-    hourNote[noteData] = $(this).prev().val();
-    console.log(hourNote[noteData])
+    //Save current note into it's selected hour
+    var currentNoteId = $(this).prev().attr("data-note");
+    currentNote[currentNoteId] = $(this).prev().val();
 
-    localStorage.setItem("notes",JSON.stringify(hourNote));
+    //Store notes into local storage
+    localStorage.setItem("storedNotes",JSON.stringify(currentNote));
+
 });
